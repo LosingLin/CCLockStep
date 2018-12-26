9255cf6c4075d0485ccefec8f737967a6eeb34f8
Lin <764837522@qq.com>
2018-12-27 00:06:00 +0800

    disconnect
    *.meta 文件回来

--- Party/server/app.js
+++ Party/server/app.js
@@ -10,9 +10,21 @@ io.on('connection', function(socket) {
     socket.emit('connected', {id: socket.id});
     // socket.emit('open', '连接成功');
 
+    // 获取用户名称
+    function getPlayerName(socketId) {
+        for (let key in g_onlines) {
+            if (socketId === g_onlines[key].socket.id) {
+                return key;
+            }
+        }
+    }
+
     socket.on('disconnect', function() {
         //socket.emit('disconnect', {id: socket.id});
-        console.log('断开连接 socket id: ', socket.id);        
+        console.log('断开连接 socket id: ', socket.id);
+        let playerName = getPlayerName(socket.id);
+        // g_onlines[playerName].online = false;  
+        g_onlines[playerName] = null;     
     });
 
     socket.on('joinGame', function(data) {
